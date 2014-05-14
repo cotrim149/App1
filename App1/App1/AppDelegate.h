0af6c59112e6303fc4cda4b5ea3b27b146464df4
Victor Cotrim <cotrim149@gmail.com>
2014-05-14 16:08:50 -0300

    Commit inicial com imagem de ícones, de abertura e parte gráfica

---
+++ App1/App1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  App1
+//
+//  Created by Victor de Lima on 14/05/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  App1
+//
+//  Created by Victor de Lima on 14/05/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
